4a7d98a5190913d171d758b5ecd1d52b140b8dbf
AndreSakti <49118968+AndreSakti@users.noreply.github.com>
2023-05-18 14:09:53 +0700

    Update menu-vless.sh

--- menu-vless.sh
+++ menu-vless.sh
@@ -47,8 +47,8 @@ export WARNING="${RED}\e[5m"
 export UNDERLINE="\e[4m"
 
 # // Exporting URL Host
-export Server_URL="raw.githubusercontent.com/andresaktia/test/main"
-export Server1_URL="raw.githubusercontent.com/andresaktia/limit/main"
+export Server_URL="raw.githubusercontent.com/AndreSakti/test/main"
+export Server1_URL="raw.githubusercontent.com/AndreSakti/limit/main"
 export Server_Port="443"
 export Server_IP="underfined"
 export Script_Mode="Stable"
